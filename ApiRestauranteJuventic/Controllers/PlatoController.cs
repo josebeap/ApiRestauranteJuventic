beec1dfacefebb942496c9102ed31601299015c1
Jose Luis Beltran Aponte <josebeap99@gmail.com>
2021-11-30 22:17:55 -0500

    Controlador y modelo Plato

---
+++ ApiRestauranteJuventic/Controllers/PlatoController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiRestauranteJuventic.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlatoController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IWebHostEnvironment _env;
+        public PlatoController(IConfiguration configuration, IWebHostEnvironment env)
+        {
+            _configuration = configuration;
+            _env = env;
+        }
+
+
+        // GET: api/<RestauranteController>
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = @"
+                        select id,nombre,precio,descripcion,img, id_restaurante from plato
+            ";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("basejuventic");
+            MySqlDataReader myReader;
+            using (MySqlConnection mycon = new MySqlConnection(sqlDataSource))
+            {
+                mycon.Open();
+                using (MySqlCommand myCommand = new MySqlCommand(query, mycon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    mycon.Close();
+                }
+            }
+
+            return new JsonResult(table);
+        }
+    }
+}
---
+++ ApiRestauranteJuventic/Models/Plato.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiRestauranteJuventic.Models
+{
+    public class Plato
+    {
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public int precio { get; set; }
+        public string descripcion { get; set; }
+        public string img { get; set; }
+        public string id_restaurante { get; set; }
+    }
+}
